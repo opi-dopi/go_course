8c620a6b735f4d46882f0a1f618436ea185d3df9
Panaramix <panaramix@example.com>
2019-11-15 16:07:39 +0300

    lesson 2 home task 1

---
+++ home2_1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(factorial(6))
+}
+
+func factorial(i uint) uint {
+
+	var fact uint = 1
+	for i > 0 {
+		fact *= i
+		i--
+	}
+	return fact
+}
