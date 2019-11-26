390c2c7fb151db015a0ae07b325a79a314f702e0
opi-dopi <gnom606@mail.ru>
2019-11-26 13:54:33 +0300

    Update home_3_3.go

--- home_3_3.go
+++ home_3_3.go
@@ -9,11 +9,11 @@ func main() {
 	a := map[int]string{10: "asd", 0: "qweqw", 500: "werwer"}
 	b := map[int]string{2: "a", 0: "b", 1: "c"}
 
-	fmt.Println(printSorted(a))
-	fmt.Println(printSorted(b))
+	printSorted(a)
+	printSorted(b)
 }
 
-func printSorted(x map[int]string) map[int]string {
+func printSorted(x map[int]string) {
 
 	newMap := make(map[int]string)
 	arKeys := make([]int, 0, len(x))
@@ -28,6 +28,6 @@ func printSorted(x map[int]string) map[int]string {
 		newMap[v] = x[v]
 	}
 
-	return newMap
+	fmt.Println(newMap)
 
 }
