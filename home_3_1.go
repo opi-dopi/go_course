9e485c199a14a213e101b9fee614e4b08aecfc2e
opi-dopi <gnom606@mail.ru>
2019-11-26 14:30:21 +0300

    Update home_3_1.go

--- home_3_1.go
+++ home_3_1.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(average(slice))
+	var x1 []int
+	x2 := []int{10, 20}
+	x3 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(average(x1))
+	fmt.Println(average(x2))
+	fmt.Println(average(x3))
 }
 
-func average(slice []int) float64 {
-	summ := 0
-	for _, val := range slice {
-		summ += val
+func average(arr []int) (float64, error) {
+
+	if len(arr) == 0 {
+		return float64(0), errors.New("Array length cannot be 0")
 	}
-	return float64(summ) / float64(len(slice))
+
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+
+	return float64(sum) / float64(len(arr)), nil
 }
